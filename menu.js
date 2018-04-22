cb90ec9bbb444c2dc64d0115322ac6f6742c4aa7
Elunn <Elunn@users.noreply.github.com>
2018-04-22 21:04:58 +0100

    new website upload

--- menu.js
+++ menu.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
 
     $(".burger").on("click", function(){
-        $("nav ul").toggleClass("open");
+        $("nav").toggleClass("open");
         
     });
     
 });
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
